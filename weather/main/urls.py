473f182bd06617cc7f5f1c72c9e0990eb43e457f
Vladimir <vovchikp.1989@gmail.com>
2021-08-23 20:11:48 +0300

    serializers done & endpoint done

--- weather/main/urls.py
+++ weather/main/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework import routers
 
-urlpatterns=[
-    path('',views.index),
+from . import views
+from .views import CityViewSet
+
+router = routers.DefaultRouter()
+router.register(r'city', CityViewSet)
+
+urlpatterns = [
+    path('', views.index),
+    path('v1/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
 ]
--- weather/main/views.py
+++ weather/main/views.py
@@ -3,7 +3,14 @@ from datetime import datetime
 
 import requests
 from django.shortcuts import render
+
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+
 from .models import City
+from .serializers import CitySerializer
 
 WEATHER_KEY = os.getenv('WEATHER_KEY')
 
@@ -42,3 +49,13 @@ def index(request):
 
     context = {'weather_data': city_weather}
     return render(request, 'weather.html', context)
+
+
+class CityViewSet(ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    permission_classes = [IsAuthenticated]
+    filter_fields = ['city']
+    search_fields = ['city', 'temperature']
+    ordering_fields = ['temperature']
